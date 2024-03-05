eb0c372982f3299102d14f6bacf21d477e403f65
yamshita <161791142+yamshita@users.noreply.github.com>
2024-03-05 11:54:13 +0900

    Even and odd
    0 or even or odd

--- main.c
+++ main.c
@@ -2,6 +2,26 @@
 
 int main(void)
 {
-    printf("hello, world\n");
+    int num;
+    
+    printf("数字を入力してください：");
+    scanf("%d", &num);
+    
+    if(num != 0)
+    {
+        if ((num % 2) == 0)
+        {
+           printf("入力した数字%dは偶数です。\n", num);
+        }
+        else
+        {
+            printf("入力した数字%dは奇数です。\n", num);
+        }
+    }
+    else
+    {
+        printf("-------------\n");
+    }
+
     return 0;
 }
