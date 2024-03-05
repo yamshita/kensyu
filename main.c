e37a35b947e706ab91abec799f687f0ff39a7013
yamshita <161791142+yamshita@users.noreply.github.com>
2024-03-05 14:43:18 +0900

    ensyu2
    hannbetu

--- main.c
+++ main.c
@@ -2,16 +2,34 @@
 
 int main(void)
 {
-    int num;
+    int num, i;
     
     printf("数字を入力してください：");
     scanf("%d", &num);
     
+    // 判定１の処理
+    if(num == 0)
+    {
+        printf("入力された数字は0です。\n");
+    }
+    else
+    {
+        if (num > 0 ) 
+        {
+            printf("入力した数字%dは正の整数です。\n", num);
+        }
+        else
+        {
+            printf("入力した数字%dは負の整数です。\n", num);
+        }
+    }
+
+    // 判定２の処理 
     if(num != 0)
     {
         if ((num % 2) == 0)
         {
-           printf("入力した数字%dは偶数です。\n", num);
+            printf("入力した数字%dは偶数です。\n", num);
         }
         else
         {
@@ -23,5 +41,16 @@ int main(void)
         printf("-------------\n");
     }
 
+    // 判定３の処理
+    for(i = 2; i < num; i++) 
+    {
+        if(num % i == 0) 
+        {
+            printf("%d は素数ではありません。\n", num);
+            return 0;
+        }
+    }
+
+    printf("%d は素数です。\n", num);
     return 0;
 }
