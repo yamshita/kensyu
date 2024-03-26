40bad842480bd67b3f2b3d66b70d226f1322eb26
yamashita <yamashita@ace-software.com>
2024-03-26 12:25:28 +0900

    これは練習。

---
+++ main1.py
@@ -0,0 +1,10 @@
+num1 = int(input('seisu nyuryoku:'))
+num2 = int(input('seisu nyuryoku2:'))
+num3 = int(input('seisu nyuryoku3:'))
+
+a = [num1, num2, num3]
+
+ans = sum(a)
+
+print('answer is :', ans)
+
